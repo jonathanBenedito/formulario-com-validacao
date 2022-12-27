65a7765ee64606920ac467334ed812b23cddc3b3
PatchesTheWright <jonathan.valbe.jb@gmail.com>
2022-12-27 18:33:09 -0300

    Subindo arquivos para o repositorio

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+function validarFormulario(event) {
+    event.preventDefault();
+    
+    for(let i = 0; i < event.target.length; i++) {
+        const campo = event.target[i];
+
+        if (!campo.value) {
+            campo.parentElement.classList.add('invalido');
+        } else {
+            campo.parentElement.classList.remove('invalido');
+            campo.parentElement.classList.add('valido');
+        }
+    }
+}
